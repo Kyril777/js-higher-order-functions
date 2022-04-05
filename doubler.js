d7ea2b602a52341e47b9223b44b4403bbc9b6c0d
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-04-04 23:59:18 -0400

    Update doubler.js

--- doubler.js
+++ doubler.js
@@ -4,3 +4,10 @@ const array  = [1, 2, 4];
 const doubler = array.map(function mapper(number) {
   return 2 * number;
 });
+
+
+
+let array = ['4', '8', '12'];
+let arrayDoubled = [];
+for (let i = 0; i < array.length; i++) {
+  arrayDoubled.push(arrau[i] * 2);
