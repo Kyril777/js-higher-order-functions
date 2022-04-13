63b4f1ea36ea94560ca07b20c25bba5bc9b30efe
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-04-12 23:46:15 -0400

    Update birthdate_to_ages.js

--- birthdate_to_ages.js
+++ birthdate_to_ages.js
@@ -1,7 +1,7 @@
-/* Use the map method to determine age by birthyear in 2021.  */
+/* Use the map method to determine age by birthyear in 2022.  */
 
 let determineAges = () => {
   const birthYear = [1965, 1970, 1980, 1990, 2000]
-  let ages = birthYear.map(year => 2020 - year + ' ') 
+  let ages = birthYear.map(year => 2022 - year + ' ') 
   return ages
 }
