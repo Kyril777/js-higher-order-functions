046690899297d5794f361c4fcccbdf8bc7375ae3
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-04-15 23:50:23 -0400

    Create filter_words.js

---
+++ filter_words.js
@@ -0,0 +1,14 @@
+/* Function to filter four-letter words in an array. */
+
+const filter = words => {
+  const filtered = [];
+  for (let i = 0, { length } = words; i < length; i++) {
+    const word = words[i];
+    if (word.length !== 4) filtered.push(word);
+  }
+  return filtered;
+};
+
+filter(['duck', 'free', 'dues', 'mud']);
+
+// mud
