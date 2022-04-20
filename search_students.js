b58aa058d052d9625f0d6cffb9d0ac8f8baed5c7
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-04-19 23:54:25 -0400

    Create search_students.js

---
+++ search_students.js
@@ -0,0 +1,29 @@
+const garibaldi = new Student('Garibaldi', 'June',
+     '', 'Rutgers');
+garibaldi.address = new Address('US');
+
+const colson = new Student('Colson', 'Jessica',
+     '', 'McGill');
+colson.address = new Address('Canada');
+
+const easterbrook = new Student('Easterbrook', 'Pete',
+   '', 'Rutgers');
+easterbrook.address = new Address('US');
+
+const rush = new Student('Rush', 'Sally',
+    '', 'Central Florida');
+rush.address = new Address('US');
+
+let selector = (country, school) => {
+   return function(student) { 
+      return student.address.country() === country &&
+          student.school() === school;
+     };
+}
+
+let searchStydents = function(friends, selector) {
+    return friends.filter(selector);
+};
+
+searchStudents([garibaldi, colson, easterbrook, rush], 
+  selector('Canada', 'McGill')); 
