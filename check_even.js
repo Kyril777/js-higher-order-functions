783b065585a08602ce577f5c5f4decaf6636f9d2
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-04-24 23:40:08 -0400

    Update check_even.js

--- check_even.js
+++ check_even.js
@@ -1,6 +1,6 @@
-/* Use filter to check if an array has an event digit. */
+/* Use filter to check if an array has an even digit. */
 
-let check_ven = content => content % 2 == 0
+let check_even = content => content % 2 == 0
 
 array1 = [1, 2, 3, 4, 5]
 array2 = array1.filter(check_even)
