90ed795e033f29d84b4b72de004294a9cdca0d94
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-04-30 23:40:50 -0400

    Create sort_keywords_acending.js

---
+++ sort_keywords_acending.js
@@ -0,0 +1,40 @@
+/* Sort an array in descending order using sort(). */
+
+const keywords = [
+    {
+        key: 22,
+        value: 'hammer',
+    },
+    {
+        key: 25,
+        value: 'quarks',
+    },
+    {
+        key: 21,
+        value: 'Omaha',
+    }
+    {
+        key: 23,
+        value: 'federal',
+    },
+    {
+        key: 24,
+        value: 'elbow',
+    },
+    {
+        key: 20,
+        value: 'latitude',
+    },
+];
+
+keywords.sort((firstValue, secondValue) => {
+    if (firstValue.key < secondValue.key) {
+        return -1;
+    } else {
+        return 1;
+    }
+});
+
+keywords.forEach((value) => {
+    console.log(`${value.key} ${value.value}`)
+});
