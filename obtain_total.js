120ebb312029eaa0c960db349de88718fb21d541
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-07-31 23:48:36 -0400

    Update obtain_total.js

--- obtain_total.js
+++ obtain_total.js
@@ -1,5 +1,11 @@
+// ES6 syntax.
 let numbers = [1, 4, 6, 8, 9];
 
-// ES6 syntax.
 numbers.reduce((accumulator, value) => accumulator + value);
 // output: 28
+
+
+// Using reduce.
+const digits = [1, 2, 3, 4, 5];
+const totalValue = digits.reduce((sum, digit) => sum + digit);
+console.log(totalValue);
