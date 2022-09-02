16bc96499db375815e0b830ddbc36217b1033a22
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-09-01 23:47:42 -0400

    Update drinking_age.js

--- drinking_age.js
+++ drinking_age.js
@@ -12,3 +12,21 @@ for (let i = 0; i < ages.length; i++){
 
 console.log(mayDrink)
 
+
+/* */
+
+
+const array = [
+  {name:"John Roe", age:21, gender:'male'},
+  {name:"Devin Tatum", age:19, gender:'male'},
+  {name:"Sterling Pope", age:45, gender:'male'},
+  {name:"Caldicott Rush", age:16, gender:'female'},
+  {name:"Pierre Champeaux", age:27, gender:'female'},
+  {name:"Jane Doe #3", age:20, gender:'female'}
+]
+
+const mayDrink2 = array.filter((person)=>{
+	return person.age < 21 ? true : false
+})
+
+console.log(mayDrink2) 
