6a5736e23b43ae80a469bef50a676c212c6ebf63
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-09-11 23:59:48 -0400

    Update combine_words.js

--- combine_words.js
+++ combine_words.js
@@ -1,5 +1,5 @@
 /* Use reduce function to form a sentence */
-const myWords = [ 'These', 'all', 'form', 'a', 'sentence' ];
+const myWords = [ 'A', 'sentence', 'is', 'formed'];
 
 const sentence = myWords.reduce( (prev, curr) => {
  return prev + ' ' + curr;
