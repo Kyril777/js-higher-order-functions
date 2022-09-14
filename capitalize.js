4a49c7ac10e9d4e004b689fd84cf0729ae14e939
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-09-13 23:59:23 -0400

    Update capitalize.js

--- capitalize.js
+++ capitalize.js
@@ -2,3 +2,9 @@
 
 const words = ["drive", "me", "crazy"];
 const capitalized = words.map((word) => word.toUpperCase());
+
+let fruits = ["apple", "orange", "cherry"
+]
+
+/* Another method. */
+const names = ['David', 'James', 'Beko'].map(name => name.toUpperCase());
