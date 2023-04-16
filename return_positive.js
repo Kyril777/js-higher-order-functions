f802e9b8cd35ad78108c89fe83fe68a11ebb0b10
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-04-15 23:54:55 -0400

    Update return_positive.js

--- return_positive.js
+++ return_positive.js
@@ -5,3 +5,18 @@ let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let pos_nums = nums.filter((e) => e > 0);
 
 return pos_nums;
+
+
+/* Use flatMap method to return only positive values. */
+
+const arr = [5, 4, -3, 20, 17, -33, -4, 18];
+
+const only_positives = arr.flatMap((n) => {
+  if (n < 0) {
+    return [];
+  } else (n > 0); {
+  	return n;
+  }
+  
+});
+console.log(only_positives); // [6, 7, 99, 91]
