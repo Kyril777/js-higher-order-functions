a9765a5a1ba686ced4ef5add22f871b1455ad48b
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-04-24 23:59:56 -0400

    Update filter_by_age.js

--- filter_by_age.js
+++ filter_by_age.js
@@ -5,9 +5,9 @@ const students = [
   {id: 443, name: "Avery", age: 18 },
   {id: 206, name: "Madison", age: 19 },
   {id: 771, name: "Jean", age: 20 },
-  {id: 905, name: "Harper", age: 19 }
+  {id: 905, name: "Harper", age: 21 }
 ];
 
 // Filter by age.
 const studentList = students.filter((student) => student.age > 21 )
-console.log(studentList); // { age: 23, id: 984, name: "Leslie" }]
+console.log(studentList); // { age: 23, id: 984, name: "Leslie" }], { age: 21, id: 905, name: "Harper" }]
