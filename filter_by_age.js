631460c8da745140df26ba54f18b098fbcd01eb9
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-04-24 23:58:08 -0400

    Create filter_by_age.js

---
+++ filter_by_age.js
@@ -0,0 +1,13 @@
+/* Use filter method to print all students are 21 or older. */
+
+const students = [
+  {id: 984, name: "Leslie", age: 23 },
+  {id: 443, name: "Avery", age: 18 },
+  {id: 206, name: "Madison", age: 19 },
+  {id: 771, name: "Jean", age: 20 },
+  {id: 905, name: "Harper", age: 19 }
+];
+
+// Filter by age.
+const studentList = students.filter((student) => student.age > 21 )
+console.log(studentList); // { age: 23, id: 984, name: "Leslie" }]
