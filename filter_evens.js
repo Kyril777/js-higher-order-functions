d1824ca58ebb710eb28ce7c2c70c63c2bf8c6429
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-04-26 23:41:42 -0400

    Update filter_evens.js

--- filter_evens.js
+++ filter_evens.js
@@ -4,3 +4,12 @@ const numbers = [3, 78, 99, 6, 714, 805, 31, 12, 500];
 const even = numbers.filter((numb) => numb % 2 === 0)
 
 console.log(even) // 78, 6, 714, 12, 500
+
+
+const array = [-100, -365, -42, -8, 0, 13, 99];
+
+const evens = array.filter((x) => {
+   return x % 2 === 0;
+})
+
+console.log(evens); // [-100, -42, -8, 0]
