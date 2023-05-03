1c5214e8250b6f6f2ec86b6d17f4c3566fab4302
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-05-02 23:26:03 -0400

    Update filter_number_array.js

--- filter_number_array.js
+++ filter_number_array.js
@@ -1,6 +1,14 @@
-/* Find all numbers less than 10. */
+const randomNumbers = [4, 11, 42, 14, 39, 20];
 
-const randomNumbers = [4, 11, 42, 14, 39];
-const filteredArray = randomNumbers.filter(n => {  
-  return n < 10;
+/* Use filter() to find all numbers less than 20. */
+const lessThanTwenty = randomNumbers.filter(n => {  
+  return n <= 20;
 });
+
+/* Use filter() to find all numbers divisible by 3. */
+const divisibleByThree = randomNumbers.filter(n => { 
+  return n % 3 === 0
+});
+
+console.log(lessThanTwenty); // [4, 11, 14, 20]
+console.log(divisibleByThree); // [42, 39]
